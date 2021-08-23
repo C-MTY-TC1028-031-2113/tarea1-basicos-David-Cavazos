1bf0be7c88e2f28e9623314e5c96ddc3981cc9ee
user <A01721909@itesm.mx>
2021-08-22 23:09:28 -0500

    tarea1ejercicio4

--- assignments/04PEMDAS/src/exercise.py
+++ assignments/04PEMDAS/src/exercise.py
@@ -5,7 +5,7 @@ def main():
     a=4
     b=5
     oper3=(a**3 + 2*b**2) / (4*a)
-    oper4 = (2*(a+b)**2 + 4*(a-b)**2) / (a*b)**2
+    oper4 = ((2*((a+b)**2) + 4*((a-b)**2)) / (a*b**2))
     oper5 = math.sqrt((a+b)**2 + 2**(a+b)) / (2*a + 2*b)**2
     print(round(oper1,4))
     print(round(oper2,4))
